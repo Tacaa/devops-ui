062b1f74e5515099f1b3f900a13f65a470e4387e
n-vojin <80399345+n-vojin@users.noreply.github.com>
2025-02-02 20:29:58 +0100

    feat: add header component

---
+++ devops-ui/src/app/header/right-menu/right-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-menu',
+  templateUrl: './right-menu.component.html',
+  styleUrls: ['./right-menu.component.css'],
+})
+export class RightMenuComponent {
+  userType: 'GUEST' | 'HOST' | null = null; // Default: not logged in
+
+  constructor() {
+    // Example: Fetch user type from localStorage or API
+    //this.userType = localStorage.getItem('userType') as 'GUEST' | 'HOST' | null;
+    this.userType = 'HOST';
+  }
+}
