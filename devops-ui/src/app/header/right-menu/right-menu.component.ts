223c721e5890772ca122ce9a135b2449adec99b1
n-vojin <80399345+n-vojin@users.noreply.github.com>
2025-04-11 19:07:39 +0200

    feat: add unread notification counter

--- devops-ui/src/app/header/right-menu/right-menu.component.ts
+++ devops-ui/src/app/header/right-menu/right-menu.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { CurrentUser, Role } from 'src/app/shared/models/user.model';
 import { Subscription } from 'rxjs';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { Notification } from 'src/app/shared/models/notification.model';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-right-menu',
@@ -12,16 +15,31 @@ export class RightMenuComponent implements OnInit, OnDestroy {
   userType: Role | undefined = undefined;
   currentUser: CurrentUser | null = null;
   private userSubscription!: Subscription;
+  unreadCount: number = 0;
+  private routerSubscription!: Subscription;
 
   // ✅ Expose Role enum for the template
   public Role = Role;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.userSubscription = this.authService.currentUser$.subscribe((user) => {
       this.currentUser = user;
       this.userType = user?.role;
+    });
+
+    this.loadUnreadCount();
+
+    // Listen for route changes
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loadUnreadCount(); // Reload unread notifications on route change
+      }
     });
   }
 
@@ -29,7 +47,18 @@ export class RightMenuComponent implements OnInit, OnDestroy {
     this.authService.logout();
   }
 
+  loadUnreadCount() {
+    this.notificationService
+      .getUserNotifications()
+      .subscribe((notifications: Notification[]) => {
+        this.unreadCount = notifications.filter((n) => !n.read).length;
+      });
+  }
+
   ngOnDestroy(): void {
     this.userSubscription.unsubscribe();
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 }
